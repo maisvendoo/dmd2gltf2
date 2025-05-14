c9fdbdc571c545c9dcc6d06fcf4686f3dfb3bda7
maisvendoo <maisvendoo@gmail.com>
2025-05-14 16:04:49 +0300

    Убрал каталог old за ненадобностью

--- old/include/cmake_defines.h
+++
@@ -1,4 +0,0 @@
-#define DMD_MODELS_DIR "/home/ksv/work-ANI/Projects/ANI/dmd2gltf/models/dmd"
-#define GLTF_MODELS_DIR "/home/ksv/work-ANI/Projects/ANI/dmd2gltf/models/gltf"
-#define DMD_ROUTES_DIR "/home/ksv/work-ANI/Projects/ANI/dmd2gltf/routes/dmd"
-#define GLTF_ROUTES_DIR "/home/ksv/work-ANI/Projects/ANI/dmd2gltf/routes/gltf"
